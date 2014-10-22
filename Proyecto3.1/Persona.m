3694d2da21cce68aa5b19ebcbc94d49ff551dbee
alumno <alumno@alumnos-Mac.local>
2014-10-22 10:04:29 +0200

    a

---
+++ Proyecto3.1/Persona.m
@@ -0,0 +1,32 @@
+//
+//  Persona.m
+//  Proyecto3.1
+//
+//  Created by alumno on 22/10/14.
+//  Copyright (c) 2014 javi. All rights reserved.
+//
+
+#import "Persona.h"
+
+@implementation Persona
+
+-(id)initWithParams:(NSString *)nombre primerApellido:(NSString *)primerApellido anyoNacimiento:(NSNumber *)anyoNacimiento{
+    
+    self = [super init];
+    self.nombre = nombre;
+    self.primerApellido = primerApellido;
+    self.anyoNacimiento = anyoNacimiento;
+    
+    return self;
+}
+
+-(NSString*)saluda{
+    return @"Holaaaaaa!!!";
+}
+
+-(void)diAlgo{
+    [self saluda];
+    NSLog(@"%@ %@ %@",[self saluda] ,self.nombre, self.primerApellido);
+}
+
+@end
--- Proyecto3.1/main.m
+++ Proyecto3.1/main.m
@@ -8,9 +8,22 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "Persona.h"
+#import "Persona2.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        
+        Persona *persona = [[Persona alloc] initWithParams:@"Nombre" primerApellido:@"Apellido" anyoNacimiento:@2000];
+        [persona diAlgo];
+        
+        Persona2 *persona2 = [[Persona2 alloc] initWithParams:@"Persona2" primerApellido:@"Apellido2" anyoNacimiento:@2500];
+        [persona2 diAlgo];
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+    
+    
+    
+    
 }
